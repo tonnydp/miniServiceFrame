b104227eb5b7ec78cc01413a80ebf10660708866
tonnydp <kissu521@163.com>
2016-06-29 21:56:23 +0800

    add empty project

---
+++ miniServiceFrame/src/main/java/info/tonnydp/miniServiceFrame/App.java
@@ -0,0 +1,13 @@
+package info.tonnydp.miniServiceFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
